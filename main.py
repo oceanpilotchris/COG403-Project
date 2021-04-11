a0e0ee7a8a04c9241b1f2f04a0be00e490f48968
(Carrie) Cen Hong <cen.hong@mail.utoronto.ca>
2021-04-11 13:21:32 -0400

    can't figure out get_week_ids

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ csv_file = open("sample Hot Stuff.csv", "r")
 # read csv file as a dictionaries, you can access each dictionary by iterating
 # through them.
 csv_reader = csv.DictReader(csv_file)
-
+lines = len(list(csv_reader))
 
 # A bunch of basic getters
 def get_week_ids() -> list:
@@ -43,22 +43,24 @@ def get_year_published(week_id: str) -> int:
 # the keys are the strings of the year of the songs, and the values are lists of
 # (song name, song artist).
 # step 1:
-# def get_i_by_year(first_year: int, last_year: int) -> List[int]:
-#     """
-#     Extracts songs from first_year-last_year and return them by their index in Hot Stuff.csv
-#     :param first_year: the first year to be included
-#     :param last_year: the last year to be included. last_year >= first_year
-#     :return: a list of the desired songs' index in Hot Stuff
-#     """
-#     indexes = []
-#     for i in range(len(get_week_ids())):
-#         if first_year <= get_year_published(get_week_ids()[i]) <= last_year:
-#             indexes.append(i)
-#     return indexes
-#
-# print(get_i_by_year(2008, 2019))
-
+def get_i_by_year(first_year: int, last_year: int) -> List[int]:
+    """
+    Extracts songs from first_year-last_year and return them by their index in Hot Stuff.csv
+    :param first_year: the first year to be included
+    :param last_year: the last year to be included. last_year >= first_year
+    :return: a list of the desired songs' index in Hot Stuff
+    """
+    indexes = []
+    for i in range(lines):
+        if first_year <= get_year_published(get_week_ids()[i]) <= last_year:
+            indexes.append(i)
+    return indexes
 print(get_week_ids())
+# print(get_i_by_year(2008, 2019))
+# song_info = {}
+#
+#
+# print(song_info)
 # step 2: create the desired dictionary
 
 
@@ -73,7 +75,7 @@ genius = lyricsgenius.Genius(token, verbose=False)
 song = genius.search_song('California Gurls', artist="Katy Perry Featuring Snoop Dogg")
 # TODO: clean up the lyrics using regex.
 lyrics = re.sub("\n", " ", song.lyrics)
-print(song.lyrics)
+# print(song.lyrics)
 # TODO: add it to the dictionary
 
 csv_file.close()
